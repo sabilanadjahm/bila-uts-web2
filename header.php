3cdaaa1ccbe92f9feea3ffc0d19593a33ab411b9
sabilanadjahm <sabilahatimah@gmail.com>
2025-04-23 09:28:34 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sistem PMB Masoem University</title>
+  <link rel="stylesheet" href="/uts_lancar/assets/style.css"> <!-- pakai path absolut -->
+  <style>
+    nav {
+      background-color: #004080;
+      padding: 10px;
+    }
+    nav ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 15px;
+    }
+    nav ul li a {
+      color: white;
+      text-decoration: none;
+      font-weight: bold;
+    }
+    nav ul li a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="/uts_lancar/index.php">Beranda</a></li>
+      <li><a href="/uts_lancar/pages/data_pendaftaran.php">Data Pendaftaran</a></li>
+      <li><a href="/uts_lancar/pages/input_data_mahasiswa.php">Input/Edit Mahasiswa</a></li>
+      <li><a href="/uts_lancar/pages/verifikasi_dokumen.php">Verifikasi Dokumen</a></li>
+      <li><a href="/uts_lancar/pages/jadwal_tes.php">Jadwal Tes</a></li>
+      <li><a href="/uts_lancar/pages/lihat_jadwal_tes.php">Jadwal Tes 2</a></li>
+      <li><a href="/uts_lancar/pages/input_hasil_tes.php">Input nilai</a></li>
+      <li><a href="/uts_lancar/pages/kelulusan.php">Hasil Tes & Kelulusan</a></li>
+      <li><a href="/uts_lancar/pages/notifikasi_pendaftar.php">Notifikasi Pendaftar</a></li>
+      <li><a href="/uts_lancar/pages/laporan_harian.php">Laporan Harian</a></li>
+      <li><a href="/uts_lancar/pages/manajemen_akun.php">Manajemen Petugas</a></li>
+      <li><a href="/uts_lancar/pages/setting_periode.php">Setting Periode</a></li>
+      <li><a href="/uts_lancar/pages/setting_biaya_pendaftaran.php">Setting Gelombang</a></li>
+      <li><a href="/uts_lancar/pages/verifikasi_pembayaran.php">Biaya Pendaftaran</a></li>
+      <li><a href="/uts_lancar/pages/cetak_kartu.php">Cetak Kartu & Bukti</a></li>
+      <li><a href="/uts_lancar/pages/laporan_akhir.php">Laporan Akhir</a></li>
+    </ul>
+  </nav>
